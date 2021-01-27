1d6ad4dff1d50cc7651e97348b16733a47072893
estrangedwriter <jeromezhang1996@gmail.com>
2021-01-27 14:10:36 -0800

    To-Do App nearing completion

--- DjangoApplication/todo_list/urls.py
+++ DjangoApplication/todo_list/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     path('to-do/', views.To_Do, name = 'To_Do'),
     path('about/', views.about, name = 'about'),
     path('contact/', views.contact, name = 'contact'),
+    path('delete/<list_id>', views.delete, name='delete'),
+    path('complete/<list_id>', views.complete, name='complete'),
+    path('incomplete/<list_id>', views.incomplete, name='incomplete'),
 ]
--- DjangoApplication/todo_list/views.py
+++ DjangoApplication/todo_list/views.py
@@ -1,13 +1,51 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import List
+from .forms import ListForm
+from django.contrib import messages
+from django.http import HttpResponseRedirect
+
 
 def home(request):
     return render(request, 'home.html', {})
 
 def To_Do(request):
-    return render(request, 'To_Do.html', {})
+    
+    if request.method == "POST":
+        form = ListForm(request.POST or None)
+
+        if form.is_valid():            
+            form.save()
+            all_items = List.objects.all        
+            messages.success(request, ('Item has been added to the list!'))
+            return render(request, 'To_Do.html', {'all_items': all_items})        
+    
+    else:
+        all_items = List.objects.all
+        return render(request, 'To_Do.html', {'all_items': all_items})
+
+def delete(request, list_id):
+    item = List.objects.get(pk = list_id)
+    item.delete()
+    messages.success(request, ('Item has been deleted!'))
+    return redirect('To_Do')
+
+def complete(request, list_id):
+    item = List.objects.get(pk = list_id)
+    item.completed = True
+    item.save()
+    messages.success(request, ('Item has been completed!'))
+    return redirect('To_Do')
+
+def incomplete(request, list_id):
+    item = List.objects.get(pk = list_id)
+    item.completed = False
+    item.save()
+    messages.success(request, ('Item status has been changed.'))
+    return redirect('To_Do')
 
 def about(request):
-    return render(request, 'about.html', {})
+    fullname = "Jerome Zhang"
+    return render(request, 'about.html', {'name': fullname})
 
 def contact(request):
     return render(request, 'contact.html', {})
